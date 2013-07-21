822ae9c1efd44aa52842333b02594eb6021e4ba2
Asfarial <xxxkova@gmail.com>
2013-07-21 22:17:36 +0300

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Timing1.rc
+//
+#define IDD_DIALOG1                     101
+#define IDD_DIALOG2                     102
+#define IDI_ICON2                       112
+#define IDI_ICON3                       113
+#define IDB_BITMAP1                     118
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        119
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
