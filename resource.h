154ca0abc593d63be90f9ad094cce01342ce7322
Asfarial <xxxkova@gmail.com>
2013-07-24 20:46:20 +0300

    Update resource.h

--- resource.h
+++ resource.h
@@ -4,15 +4,15 @@
 //
 #define IDD_DIALOG1                     101
 #define IDD_DIALOG2                     102
-#define IDI_ICON2                       112
-#define IDI_ICON3                       113
 #define IDB_BITMAP1                     118
+#define IDB_BITMAP2                     124
+#define IDI_ICON1                       129
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        119
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
