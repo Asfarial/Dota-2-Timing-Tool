4304dc3a72a831750b363455ee0c3db538bb402c
Asfarial <xxxkova@gmail.com>
2013-07-24 20:46:01 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,6 @@
 #include <windows.h>
 #include <tchar.h>
 #include <sapi.h>
-#include "optionsIO.h"
 #include "resource.h"
 #pragma comment(linker,"\"/manifestdependency:type='win32' \
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
@@ -24,10 +23,13 @@ RECT wr = {0, 0, 720, 790};    // set the size, but not the position
 HWND zeroBoxText[10];
 HWND zeroBoxTime[10];
 HWND NUM[10];
+HWND NUMclear[10];
 HWND NUMactive[10];
 HWND timeLeft[10];
 HANDLE hThrd[10];
 DWORD ThrdID[10];
+HGDIOBJ hFont;
+HWND hTxtDest;
 HRESULT hr;
 ISpVoice * pVoice = NULL;
 bool allowed=true;
@@ -41,6 +43,7 @@ bool allowed=true;
 #define IDT_INPUT_TIMER 1104
 #define IDR_MENU_FAQ 1105
 #define IDR_MENU_ABOUT 1106
+#define IDR_MENU_OPTIONS 1107
 #define ZERO_NUM 1000
 #define ONE_NUM 1001
 #define TWO_NUM 1002
@@ -62,6 +65,16 @@ bool allowed=true;
 #define SEVEN_BOX_STOP 1018
 #define EIGHT_BOX_STOP 1019
 #define NINE_BOX_STOP 1020
+#define ZERO_CLEAR 121
+#define ONE_CLEAR 122
+#define TWO_CLEAR 123
+#define THREE_CLEAR 124
+#define FOUR_CLEAR 125
+#define FIVE_CLEAR 126
+#define SIX_CLEAR 127
+#define SEVEN_CLEAR 128
+#define EIGHT_CLEAR 129
+#define NINE_CLEAR 110
 #define WM_STOP_TIMER_MSG (WM_USER+1)
 #pragma endregion IDs
 #pragma region functions
@@ -94,6 +107,7 @@ public:
 class Text
 {
 	friend void changeBeginned(int);
+	friend void checkBeginned(int);
 	friend class ThreadJob;
 public:
 	Text()
@@ -138,12 +152,18 @@ public:
 		if(applied!=true)
 			return;
 		getText();
+		setTrue();
 		hr = pVoice->Speak(retStruct.speech, SPF_IS_XML|SPF_ASYNC, 0);
 		ZeroMemory(&retStruct.speech, 120);
 		if(beginned==false)
 			beginThread();
 	}
 protected:
+	VOID setTrue()
+	{
+		SetWindowText(zeroBoxText[number], retStruct.text);
+		SetWindowText(zeroBoxTime[number], retStruct.stdTime);
+	}
 	VOID getMainText()
 	{
 		ZeroMemory(&retStruct.text, 30);
@@ -202,11 +222,11 @@ VOID wrkWndw()
 	wcex2.style          = CS_HREDRAW | CS_VREDRAW;
 	wcex2.lpfnWndProc    = WndProc2;
 	wcex2.hInstance      = hInst;
-	wcex2.hIcon          = LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON2));
+	wcex2.hIcon          = LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON1));
 	wcex2.hCursor        = LoadCursor(NULL, IDC_ARROW);
 	wcex2.hbrBackground  = (HBRUSH)(COLOR_WINDOW);
 	wcex2.lpszClassName  = szWindowClass2;
-	wcex2.hIconSm        = LoadIcon(wcex2.hInstance, MAKEINTRESOURCE(IDI_ICON3));
+	wcex2.hIconSm        = LoadIcon(wcex2.hInstance, MAKEINTRESOURCE(IDI_ICON1));
 	if (!RegisterClassEx(&wcex2))
 	{MessageBox(NULL,_T("Call to RegisterClassEx failed!"),_T("Bad news:"),NULL);
 	}
@@ -230,7 +250,10 @@ VOID wrkWndw()
 	}
 	hMenu = CreateMenu();
 	HMENU menu_help = CreatePopupMenu();
+	HMENU menu_options = CreatePopupMenu();
 	AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)menu_help, L"&Help");
+	AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)menu_options, L"&Options");
+	AppendMenu(menu_options, MF_STRING, IDR_MENU_OPTIONS, L"Voice Options");
 	AppendMenu(menu_help, MF_STRING, IDR_MENU_FAQ, L"FAQ");
 	AppendMenu(menu_help, MF_STRING, IDR_MENU_ABOUT, L"About");
 	SetMenu(window2, hMenu);
@@ -256,6 +279,16 @@ void changeBeginned(int v)
 {
 	TextO[v].beginned=false;
 }
+void checkBeginned(int v)
+{
+	if(TextO[v].beginned==false)
+	{
+		SetWindowText(zeroBoxText[v], L"");
+		SetWindowText(zeroBoxTime[v], L"");
+		TextO[v].applied=false;
+	}
+	return;
+}
 VOID CALLBACK TimerCallBack(HWND hWnd, UINT message, UINT idTimer, DWORD dwTime)
 {
 	allowed=true;
@@ -274,11 +307,11 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	wcex.style          = CS_HREDRAW | CS_VREDRAW;
 	wcex.lpfnWndProc    = WndProc1;
 	wcex.hInstance      = hInstance;
-	wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON2));
+	wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
 	wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
 	wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW);
 	wcex.lpszClassName  = szWindowClass;
-	wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_ICON3));
+	wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_ICON1));
 
 	if (!RegisterClassEx(&wcex))
 	{MessageBox(NULL,_T("Call to RegisterClassEx failed!"),_T("Bad news:"),NULL);
@@ -427,21 +460,15 @@ LRESULT CALLBACK WndProc1(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		break;
 	case WM_CREATE:
 		{
+
 			hdc = BeginPaint(hWnd, &ps);
 			GetTextExtentPoint32(hdc, _T("Choose Your Destiny:"), lstrlen(_T("Choose Your Destiny:")), &sz);
 			EndPaint(hWnd, &ps);
 			snglBtn = CreateWindowEx(NULL,L"BUTTON",L"Single",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,20,sz.cy+80,110,35,hWnd,(HMENU)SINGLE_BUTTON, NULL,NULL);
 			hostBtn = CreateWindowEx(NULL,L"BUTTON",L"Host",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,212,sz.cy+80,110,35,hWnd,(HMENU)HOST_BUTTON, NULL,NULL);
 			clntBtn = CreateWindowEx(NULL,L"BUTTON",L"Client",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,404,sz.cy+80,110,35,hWnd,(HMENU)CLIENT_BUTTON, NULL,NULL);
-		}
-		break;
-	case WM_PAINT:
-		{
-			hdc = BeginPaint(hWnd, &ps);
-			GetTextExtentPoint32(hdc, L"Choose Your Destiny:", lstrlen(L"Choose Your Destiny:"), &sz);
-			EndPaint(hWnd, &ps);
-			HGDIOBJ hFont = CreateFont(48, 0, 0, 0, FW_REGULAR, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_TT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"Times New Roman");
-			HWND hTxtDest = CreateWindowEx(NULL, L"STATIC", L"Choose Your Destiny:", WS_CHILD|WS_VISIBLE, 225-(sz.cx), 20, 500, 48, hWnd, NULL, NULL, NULL);
+			hFont = CreateFont(48, 0, 0, 0, FW_REGULAR, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_TT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"Times New Roman");
+			hTxtDest = CreateWindowEx(NULL, L"STATIC", L"Choose Your Destiny:", WS_CHILD|WS_VISIBLE, 225-(sz.cx), 20, 500, 48, hWnd, NULL, NULL, NULL);
 			SendMessage(hTxtDest,WM_SETFONT,(WPARAM)hFont,MAKELPARAM(FALSE,0));
 		}
 		break;
@@ -475,7 +502,7 @@ LRESULT CALLBACK WndProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			int y = 105;
 			hdc = BeginPaint(hWnd, &ps);
 			GetTextExtentPoint32(hdc, _T("Button"), lstrlen(_T("Button")), &sz);
-			CreateWindowEx(NULL, L"STATIC", L"Button", ES_CENTER|WS_CHILD|WS_VISIBLE, (100-sz.cx)/2+20, 10, sz.cx, sz.cy, hWnd, NULL, NULL, NULL);
+			CreateWindowEx(NULL, L"STATIC", L"Button", ES_CENTER|WS_CHILD|WS_VISIBLE, (100-sz.cx)/2+10, 10, sz.cx, sz.cy, hWnd, NULL, NULL, NULL);
 			GetTextExtentPoint32(hdc, _T("Text"), lstrlen(_T("Text")), &sz);
 			CreateWindowEx(NULL, L"STATIC", L"Text", ES_CENTER|WS_CHILD|WS_VISIBLE, (200-sz.cx)/2+140, 10, sz.cx, sz.cy, hWnd, NULL, NULL, NULL);
 			GetTextExtentPoint32(hdc, _T("Time\n(seconds)"), lstrlen(_T("Time\n(seconds)")), &sz);
@@ -487,16 +514,36 @@ LRESULT CALLBACK WndProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			CreateWindowEx(NULL, L"STATIC", L"Time left", ES_CENTER|WS_CHILD|WS_VISIBLE, 470+szDeact.cx, 10, sz.cx, sz.cy, hWnd, NULL, NULL, NULL);
 			GetTextExtentPoint32(hdc, _T("000"), lstrlen(_T("000")), &szZero);
 			EndPaint(hWnd, &ps);
-			NUM[0]	= CreateWindowEx(NULL,L"BUTTON",L"0",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,20,40,100,35,hWnd,(HMENU)ZERO_NUM, NULL,NULL);
-			NUM[1]	= CreateWindowEx(NULL,L"BUTTON",L"1",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,47,95,45,45,hWnd,(HMENU)ONE_NUM, NULL,NULL);
-			NUM[2]	= CreateWindowEx(NULL,L"BUTTON",L"2",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,47,160,45,45,hWnd,(HMENU)TWO_NUM, NULL,NULL);
-			NUM[3]	= CreateWindowEx(NULL,L"BUTTON",L"3",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,47,225,45,45,hWnd,(HMENU)THREE_NUM, NULL,NULL);
-			NUM[4]	= CreateWindowEx(NULL,L"BUTTON",L"4",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,47,290,45,45,hWnd,(HMENU)FOUR_NUM, NULL,NULL);
-			NUM[5]	= CreateWindowEx(NULL,L"BUTTON",L"5",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,47,355,45,45,hWnd,(HMENU)FIVE_NUM, NULL,NULL);
-			NUM[6]	= CreateWindowEx(NULL,L"BUTTON",L"6",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,47,420,45,45,hWnd,(HMENU)SIX_NUM, NULL,NULL);
-			NUM[7]	= CreateWindowEx(NULL,L"BUTTON",L"7",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,47,485,45,45,hWnd,(HMENU)SEVEN_NUM, NULL,NULL);
-			NUM[8]	= CreateWindowEx(NULL,L"BUTTON",L"8",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,47,550,45,45,hWnd,(HMENU)EIGHT_NUM, NULL,NULL);
-			NUM[9]	= CreateWindowEx(NULL,L"BUTTON",L"9",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,47,615,45,45,hWnd,(HMENU)NINE_NUM, NULL,NULL);
+			NUM[0]	= CreateWindowEx(NULL,L"BUTTON",L"0",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,10,40,100,35,hWnd,(HMENU)ZERO_NUM, NULL,NULL);
+			NUM[1]	= CreateWindowEx(NULL,L"BUTTON",L"1",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,37,95,45,45,hWnd,(HMENU)ONE_NUM, NULL,NULL);
+			NUM[2]	= CreateWindowEx(NULL,L"BUTTON",L"2",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,37,160,45,45,hWnd,(HMENU)TWO_NUM, NULL,NULL);
+			NUM[3]	= CreateWindowEx(NULL,L"BUTTON",L"3",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,37,225,45,45,hWnd,(HMENU)THREE_NUM, NULL,NULL);
+			NUM[4]	= CreateWindowEx(NULL,L"BUTTON",L"4",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,37,290,45,45,hWnd,(HMENU)FOUR_NUM, NULL,NULL);
+			NUM[5]	= CreateWindowEx(NULL,L"BUTTON",L"5",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,37,355,45,45,hWnd,(HMENU)FIVE_NUM, NULL,NULL);
+			NUM[6]	= CreateWindowEx(NULL,L"BUTTON",L"6",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,37,420,45,45,hWnd,(HMENU)SIX_NUM, NULL,NULL);
+			NUM[7]	= CreateWindowEx(NULL,L"BUTTON",L"7",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,37,485,45,45,hWnd,(HMENU)SEVEN_NUM, NULL,NULL);
+			NUM[8]	= CreateWindowEx(NULL,L"BUTTON",L"8",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,37,550,45,45,hWnd,(HMENU)EIGHT_NUM, NULL,NULL);
+			NUM[9]	= CreateWindowEx(NULL,L"BUTTON",L"9",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,37,615,45,45,hWnd,(HMENU)NINE_NUM, NULL,NULL);
+			NUMclear[0]	= CreateWindowEx(NULL,L"BUTTON",L"",BS_DEFPUSHBUTTON|BS_ICON|WS_CHILD|WS_VISIBLE,112,45,25,25,hWnd,(HMENU)ZERO_CLEAR, NULL,NULL);
+			NUMclear[1]	= CreateWindowEx(NULL,L"BUTTON",L"",BS_DEFPUSHBUTTON|BS_ICON|WS_CHILD|WS_VISIBLE,112,105,25,25,hWnd,(HMENU)ONE_CLEAR, NULL,NULL);
+			NUMclear[2]	= CreateWindowEx(NULL,L"BUTTON",L"",BS_DEFPUSHBUTTON|BS_ICON|WS_CHILD|WS_VISIBLE,112,170,25,25,hWnd,(HMENU)TWO_CLEAR, NULL,NULL);
+			NUMclear[3]	= CreateWindowEx(NULL,L"BUTTON",L"",BS_DEFPUSHBUTTON|BS_ICON|WS_CHILD|WS_VISIBLE,112,235,25,25,hWnd,(HMENU)THREE_CLEAR, NULL,NULL);
+			NUMclear[4]	= CreateWindowEx(NULL,L"BUTTON",L"",BS_DEFPUSHBUTTON|BS_ICON|WS_CHILD|WS_VISIBLE,112,300,25,25,hWnd,(HMENU)FOUR_CLEAR, NULL,NULL);
+			NUMclear[5]	= CreateWindowEx(NULL,L"BUTTON",L"",BS_DEFPUSHBUTTON|BS_ICON|WS_CHILD|WS_VISIBLE,112,365,25,25,hWnd,(HMENU)FIVE_CLEAR, NULL,NULL);
+			NUMclear[6]	= CreateWindowEx(NULL,L"BUTTON",L"",BS_DEFPUSHBUTTON|BS_ICON|WS_CHILD|WS_VISIBLE,112,430,25,25,hWnd,(HMENU)SIX_CLEAR, NULL,NULL);
+			NUMclear[7]	= CreateWindowEx(NULL,L"BUTTON",L"",BS_DEFPUSHBUTTON|BS_ICON|WS_CHILD|WS_VISIBLE,112,495,25,25,hWnd,(HMENU)SEVEN_CLEAR, NULL,NULL);
+			NUMclear[8]	= CreateWindowEx(NULL,L"BUTTON",L"",BS_DEFPUSHBUTTON|BS_ICON|WS_CHILD|WS_VISIBLE,112,560,25,25,hWnd,(HMENU)EIGHT_CLEAR, NULL,NULL);
+			NUMclear[9]	= CreateWindowEx(NULL,L"BUTTON",L"",BS_DEFPUSHBUTTON|BS_ICON|WS_CHILD|WS_VISIBLE,112,625,25,25,hWnd,(HMENU)NINE_CLEAR, NULL,NULL);
+			SendMessage(NUMclear[0], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)LoadBitmap(hInst,MAKEINTRESOURCE(IDB_BITMAP2)));
+			SendMessage(NUMclear[1], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)LoadBitmap(hInst,MAKEINTRESOURCE(IDB_BITMAP2)));
+			SendMessage(NUMclear[2], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)LoadBitmap(hInst,MAKEINTRESOURCE(IDB_BITMAP2)));
+			SendMessage(NUMclear[3], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)LoadBitmap(hInst,MAKEINTRESOURCE(IDB_BITMAP2)));
+			SendMessage(NUMclear[4], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)LoadBitmap(hInst,MAKEINTRESOURCE(IDB_BITMAP2)));
+			SendMessage(NUMclear[5], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)LoadBitmap(hInst,MAKEINTRESOURCE(IDB_BITMAP2)));
+			SendMessage(NUMclear[6], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)LoadBitmap(hInst,MAKEINTRESOURCE(IDB_BITMAP2)));
+			SendMessage(NUMclear[7], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)LoadBitmap(hInst,MAKEINTRESOURCE(IDB_BITMAP2)));
+			SendMessage(NUMclear[8], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)LoadBitmap(hInst,MAKEINTRESOURCE(IDB_BITMAP2)));
+			SendMessage(NUMclear[9], BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)LoadBitmap(hInst,MAKEINTRESOURCE(IDB_BITMAP2)));
 			applyBtn= CreateWindow(L"BUTTON",L"Apply",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,594,700,100,35,hWnd,(HMENU)APPLY_BUTTON, NULL,NULL);
 			CreateWindowEx(NULL,L"BUTTON",L"Stop",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,624,40,70,35,hWnd,(HMENU)ZERO_BOX_STOP, NULL,NULL);
 			CreateWindowEx(NULL,L"BUTTON",L"Stop",BS_DEFPUSHBUTTON|WS_CHILD|WS_VISIBLE,624,100,70,35,hWnd,(HMENU)ONE_BOX_STOP, NULL,NULL);
@@ -603,6 +650,36 @@ LRESULT CALLBACK WndProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			{
 				TextO[i].apply();
 			}
+			break;
+		case ZERO_CLEAR:
+			checkBeginned(0);
+			break;
+		case ONE_CLEAR:
+			checkBeginned(1);
+			break;
+		case TWO_CLEAR:
+			checkBeginned(2);
+			break;
+		case THREE_CLEAR:
+			checkBeginned(3);
+			break;
+		case FOUR_CLEAR:
+			checkBeginned(4);
+			break;
+		case FIVE_CLEAR:
+			checkBeginned(5);
+			break;
+		case SIX_CLEAR:
+			checkBeginned(6);
+			break;
+		case SEVEN_CLEAR:
+			checkBeginned(7);
+			break;
+		case EIGHT_CLEAR:
+			checkBeginned(8);
+			break;
+		case NINE_CLEAR:
+			checkBeginned(9);
 			break;
 		default:
 			break;
