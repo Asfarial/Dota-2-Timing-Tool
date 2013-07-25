9535e43c3db3fa930822db6e3825336e9dc11355
Arial <xxxkova@gmail.com>
2013-07-25 23:19:24 +0300

    Create header.h

---
+++ header.h
@@ -0,0 +1,10 @@
+#include <windows.h>
+#include "Reader.h"
+#include "resource.h"
+#include <tchar.h>
+#include <sapi.h>
+#include "IDs.h"
+#include <commctrl.h>
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
