c561ab29ff3a5405432327189d009cad56ee1698
Arial <xxxkova@gmail.com>
2013-07-25 23:17:27 +0300

    Update resource.h

--- resource.h
+++ resource.h
@@ -2,19 +2,20 @@
 // Включаемый файл, созданный в Microsoft Visual C++.
 // Используется Timing1.rc
 //
-#define IDD_DIALOG1                     101
 #define IDD_DIALOG2                     102
-#define IDB_BITMAP1                     118
-#define IDB_BITMAP2                     124
-#define IDI_ICON1                       129
+#define IDB_BITMAP1                     103
+#define IDB_BITMAP2                     104
+#define IDI_ICON1                       105
+#define IDD_DIALOG3                     106
+#define IDD_DIALOG1                     107
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
