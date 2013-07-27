82f3f11ff8213ed998bd679e75dffedcd687c6f0
Arial <xxxkova@gmail.com>
2013-07-27 16:51:25 +0300

    Update Reader.h

--- Reader.h
+++ Reader.h
@@ -5,13 +5,14 @@
 #include <tchar.h>
 namespace CReader
 {
-  class MyClass
+	class RDLL
 	{
 	public:
 		static __declspec(dllexport) TCHAR* ReadConfig();
 		static __declspec(dllexport) TCHAR* ReadConfigForWindow();
 		static __declspec(dllexport) void CreateConfig();
 		static __declspec(dllexport) void ChangeConfig(LPCWSTR dataTransfer, int len);
+		static __declspec(dllexport) TCHAR* returnVersion();
 	};
 }
 #endif
